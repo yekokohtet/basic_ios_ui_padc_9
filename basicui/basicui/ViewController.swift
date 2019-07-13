fa499bfb7446cc6a328939b5bb9548a9214e761e
Ye Ko Ko Htet <yekokohtet@mm-digital-solutions.com>
2019-07-13 16:20:58 +0630

    - implement date picker

--- basicui/basicui/ViewController.swift
+++ basicui/basicui/ViewController.swift
@@ -12,14 +12,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var tfName: UITextField!
     @IBOutlet weak var lblShowName: UILabel!
+    @IBOutlet weak var tfDateOfBirth: UITextField!
+    
+    private var datePicker: UIDatePicker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         tfName.delegate = self
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(self.selectDate(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.viewTapped(gesture:)))
+        view.addGestureRecognizer(tapGesture)
+        
+        tfDateOfBirth.inputView = datePicker
     }
-
+    
+    @objc func viewTapped(gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func selectDate(datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        tfDateOfBirth.text = formatter.string(from: datePicker.date)
+        //        view.endEditing(true)
+    }
+    
     @IBAction func btnShowName(_ sender: Any) {
         lblShowName.text = tfName.text
     }
